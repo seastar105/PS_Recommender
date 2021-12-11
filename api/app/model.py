1657e3efd22480d557eb18d5fbbf467a710ad86b
seastar105 <seastar105@gmail.com>
2021-12-11 12:03:28 +0900

    Remove stash change

--- api/app/model.py
+++ api/app/model.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from pydantic import BaseModel
 from typing import Optional, List, Dict
 
@@ -19,26 +18,4 @@ class Response(BaseModel):
     datetime: str
     handle: Optional[str]
     tag: Optional[Tag]
-=======
-from pydantic import BaseModel
-from typing import Optional, List, Dict
-
-
-class Tag(BaseModel):
-    strong: List[str]
-    weak: List[str]
-
-
-class Problem(BaseModel):
-    id: int
-    tier: int
-    tag: List[str]
-
-
-class Response(BaseModel):
-    code: int
-    datetime: str
-    handle: Optional[str]
-    tag: Optional[Tag]
->>>>>>> Stashed changes
     problems: Optional[Dict[str, List[Problem]]]
