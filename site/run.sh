cd2413d7f82aa6d01a18edbb909f751e77184686
root <raararaara@gmail.com>
2021-12-13 01:38:07 +0900

    add node server

---
+++ site/run.sh
@@ -0,0 +1,2 @@
+docker stop web_server
+docker run -d -p 80:8080 webapp
--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 # start jupyter notebook server
-sudo docker run -itd -p 8888:8888 -p 2222:22 -v $(pwd)/recsys:/opt/notebooks --name jupyter_server seastar105/buffalo_jupyter jupyter notebook
+docker run -itd -p 8888:8888 -p 2222:22 -v $(pwd)/recsys:/opt/notebooks --name jupyter_server seastar105/buffalo_jupyter jupyter notebook
 
 # start mysql server
-sudo docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -v /mnt/mysql:/var/lib/mysql --name mysql_server mysql:5.6 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
-sudo docker cp .mylogin.cnf jupyter_server:/root/
+docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -v /mnt/mysql:/var/lib/mysql --name mysql_server mysql:5.6 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker cp .mylogin.cnf jupyter_server:/root/
+
+# start api server
+bash recsys/run.sh
+
+# start web server
+bash site/run.sh
